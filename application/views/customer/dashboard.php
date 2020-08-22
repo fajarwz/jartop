5964e05f2643c25fa3f562bae54c919af3cd0f3f
fajarwz <fajarwindhuzulfikar@gmail.com>
2020-08-22 17:55:48 +0700

    add jumbotron hero main

--- application/views/customer/dashboard.php
+++ application/views/customer/dashboard.php
@@ -2,89 +2,76 @@
   <!-- Navbar -->
   <?php $this->load->view('templates_customer/navbar') ?>
 
+  <div class="jumbotron hero-main text-white align-items-center" style="background-image: url(<?php echo base_url('assets/upload/bg1.jpg') ?>);">
+
+      <div class="row">
+        <div class="col">
+          <h1 class="display-4">Tinggalkan Laptop Kentang</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p class="lead">Kamu bisa dapat laptop dengan spek yang kamu mau tanpa harus keluar biaya banyak.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#laptop-main" role="button">Cek Laptop</a>
+        </p>
+        </div>
+      </div>
+
+  </div>
+
   <!-- Page Content -->
-  <div class="container">
+  <div class="container" id="laptop-main">
 
     <div class="row">
 
-      <div class="col-12">
+      <?php foreach($laptop as $lp) : ?>
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card h-100">
 
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+            <div class="card-img-container">
+              <img class="card-img-top" src="<?php echo base_url('assets/upload/'.$lp->gambar) ?>" alt="<?php echo $lp->merk ?>">
             </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
 
-        <div class="row">
+            <div class="card-body">
+              <small>
+                <?php
+                  echo $lp->status === 'Tersedia' ? 
+                  "<span class='badge badge-primary'>Tersedia</span>" : 
+                  "<span class='badge badge-danger'>Tidak Tersedia</span>";
+                ?>
+              </small>
 
-          <?php foreach($laptop as $lp) : ?>
-            <div class="col-lg-4 col-md-6 mb-4">
-              <div class="card h-100">
+              <h4 class="card-title">
+                <a href="<?php echo base_url('customer/detail/data/').$lp->id ?>" class="stretched-link"><?php echo $lp->merk ?></a>
+              </h4>
 
-                <div class="card-img-container">
-                  <img class="card-img-top" src="<?php echo base_url('assets/upload/'.$lp->gambar) ?>" alt="<?php echo $lp->merk ?>">
-                </div>
+              <p>
+              <?php echo $lp->processor ?> 
+              | <?php echo $lp->os ?> 
+              | <?php echo $lp->storage ?> 
+              | <?php echo $lp->ram ?> 
+              </p>
 
-                <div class="card-body">
-                  <small>
-                    <?php
-                      echo $lp->status === 'Tersedia' ? 
-                      "<span class='badge badge-primary'>Tersedia</span>" : 
-                      "<span class='badge badge-danger'>Tidak Tersedia</span>";
-                    ?>
-                  </small>
+              <h5>Rp. <?php echo $lp->harga_sewa ?>/hari</h5>
 
-                  <h4 class="card-title">
-                    <a href="<?php echo base_url('customer/detail/data/').$lp->id ?>" class="stretched-link"><?php echo $lp->merk ?></a>
-                  </h4>
-
-                  <p>
-                  <?php echo $lp->processor ?> 
-                  | <?php echo $lp->os ?> 
-                  | <?php echo $lp->storage ?> 
-                  | <?php echo $lp->ram ?> 
-                  </p>
-
-                  <h5>Rp. <?php echo $lp->harga_sewa ?>/hari</h5>
-
-                
-                </div>
-                <!-- card-body  -->
-
-              </div>
-              <!-- card h-100  -->
-
-            </div>
-            <!-- col-lg-4 col-md-6 mb-4 -->
             
-          <?php endforeach ?>
+            </div>
+            <!-- card-body  -->
+
+          </div>
+          <!-- card h-100  -->
 
         </div>
-        <!-- /.row -->
-
-      </div>
-      <!-- /.col-lg-9 -->
+        <!-- col-lg-4 col-md-6 mb-4 -->
+        
+      <?php endforeach ?>
 
     </div>
     <!-- /.row -->
